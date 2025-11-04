55961dccc4559e3b310e516dc3ee41daabf77ce2
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-11-05 02:29:32 +0600

    about page done just one issu about gallary picture not show

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Link from "next/link";
+import AboutHero from "@/components/Aboutpage/AboutHero";
+import Count from "@/components/Aboutpage/Count";
+import Mission from "@/components/Aboutpage/Mission";
+import CeoMessage from "@/components/Aboutpage/CeoMessage";
+import Value from "@/components/Aboutpage/Value";
+import AboutGallery from "@/components/Aboutpage/AboutGallery";
 
 const AboutPage = () => {
+  // useEffect(() => {
+  //   window.scrollTo(0, 0); // top-left corner
+  // }, [pathname]);
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <div>
+        <div
+          className="animated-bg fixed z-50 left-0 top-7/12 -translate-y-1/2 text-white px-6 py-2 rounded-l-lg cursor-pointer shadow-2xl "
+          style={{
+            writingMode: "vertical-rl",
+            transform: "translateY(-50%) rotate(180deg)",
+          }}
+        >
+          <Link href="/events">
+            <h3 className=" outfit-semibold uppercase">Join Seminar</h3>
+          </Link>
+        </div>
+      </div>
+      <AboutHero />
 
-export default AboutPage
+      <Count />
+      <CeoMessage />
+      <Mission />
+      <Value />
+      <AboutGallery />
+    </div>
+  );
+};
+
+export default AboutPage;
